443cc47023f6363591bb5a3e478fd90e68b7aa06
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-02-22 14:33:10 +0630

    update

---
+++ app/Http/Controllers/item.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class item extends Controller
+{
+    //
+    public function item_main()
+    {
+        return view("item");
+    }
+}
